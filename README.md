239f517f90a729fed049d51e4d6b75045ba7ed4f
Mike Groseclose <mike.groseclose@gmail.com>
2014-06-22 21:00:04 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# GIT ART
+> Put some art into your [GitHub](https://github.com/) contributions calendar.
+
+## Example
+
+
+## Usage
+Clone the git-art repo locally:
+```
+git clone https://github.com/mikegroseclose/git-art.git
+```
+
+From the git-art directory run the following:
+```
+./git-art.rb <template>
+```
+where ```<template>``` is a template file in the ./templates directory.
+
+Example:
+```
+./git-art.rb templates/groseclose.txt
+```
+
+Follow the output at the end of the ./git-art.rb to update your GitHub contributions calendar.
+
+## Templates
+In the ./templates directory is an empty.txt which can be modified to create new templates.
+
+emtpy.txt contains seven rows of comma separated values.
+Each row represents a day of the week.
+Each column represents a week in the year.
+Each comma separated value represents the numbers of commits to perform on that day.
+
+## Contributing
+Help by contributing new templates.
+To have your templates added to the project submit a Pull Request using the [GitHub Pull Request Process](https://help.github.com/articles/using-pull-requests).
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT) © [Mike Groseclose](//github.com/mikegroseclose)
---
+++ git-art.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+require 'date'
+
+OUTPUT_BASE_DIR = "output"
+OUTPUT_FILE = "README.md"
+
+def load_template(file, template)
+  if template.nil?
+    puts "Error: #{__FILE__} requires a template file to run."
+    puts "To run please pass a template file (e.g. `#{__FILE__} templates/groseclose.txt`)."
+    nil
+  elsif !File.exists? template
+    puts "Error:  template file '#{template}' does not exist."
+    nil
+  else
+    file = File.read(template)
+    file.split("\n").map { |line| line.split(",") }
+  end
+end
+
+def find_directory(base_dir)
+  offset = 0
+  output_dir = nil
+  begin
+    offset += 1
+    output_dir = "#{base_dir}_#{offset}"
+  end while Dir.exists? output_dir
+  output_dir
+end
+
+def do_commit(output_file, date, c)
+  `git init` if !Dir.exists? ".git"
+  `echo " " >> #{output_file}`
+  `git add #{output_file}`
+  date_str = "#{date.strftime('%a %d %b %Y')} 12:0#{c}:00 +0000"
+  message = "commit #{c + 1} on #{date.strftime('%a %d %b %Y')} by github.com/mikegroseclose/git-commit-art"
+  `GIT_COMMITTER_DATE="#{date_str}" git commit --date "#{date_str}" -m "#{message}."`
+end
+
+def do_commits(commit_arr)
+  width = commit_arr.first.length unless commit_arr.nil?
+  height = commit_arr.length unless commit_arr.nil?
+  date = (Date.today - Date.today.wday) - (width * height) + height unless width.nil? or height.nil?
+
+  width.times do |x|
+    height.times do |y|
+      break if date.to_s > (Date.today - 1).to_s
+      count = (commit_arr[y][x]).to_i
+      count.times do |c|
+        puts do_commit(OUTPUT_FILE, date, c)
+      end
+      date = date + 1
+    end unless height.nil?
+  end unless width.nil?
+end
+
+def done_message(output_dir)
+  "====================================================================\n" +
+  "Commits completed. A new git repo has been created at ./#{output_dir}.\n" +
+  "Run the following to push your git art to github:\n" +
+  "cd ./#{output_dir}\n" +
+  "git remote add origin <github repo>\n" +
+  "git push -u origin master"
+end
+
+output_dir = find_directory OUTPUT_BASE_DIR
+commit_arr = load_template __FILE__, ARGV[0]
+
+pwd = Dir.pwd
+Dir.mkdir output_dir
+Dir.chdir output_dir
+
+puts done_message(output_dir) if do_commits commit_arr
+
+Dir.chdir pwd
